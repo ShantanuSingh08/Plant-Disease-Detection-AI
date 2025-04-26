46fc3d8a9d844376266e696565adadf4d736b62b
Shantanu Singh <shantanusingh1808@gmail.com>
2025-04-26 22:00:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -127,6 +127,7 @@ elif app_mode == "ABOUT US":
         with cols[index % 3]:
             st.markdown(f"**    **")
             st.markdown(f"**{member['name']}**")
+            st.markdown(f"**Full-Stack Developer**")
             st.markdown(f"**    **")
 
 
