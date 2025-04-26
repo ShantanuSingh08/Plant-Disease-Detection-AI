23f419a4c462a59c4b0431e4d1cc622e782326b6
Shantanu Singh <shantanusingh1808@gmail.com>
2025-04-26 22:11:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,14 +121,14 @@ elif app_mode == "ABOUT US":
          {"name": "Shantanu Singh", "linkedin": "#", "github": "#", "instagram": "#"},
     ]
 
-    # Display Team Cards
-     cols = st.columns(3)  
-     for index, member in enumerate(team_members):
-         with cols[index % 3]:
-            st.markdown(f"**    **")
-            st.markdown(f"**{member['name']}**")
-            st.markdown(f"**Full-Stack Developer**")
-            st.markdown(f"**    **")
+        # Display Team Cards
+         cols = st.columns(3)  
+             for index, member in enumerate(team_members):
+                 with cols[index % 3]:
+                    st.markdown(f"**    **")
+                    st.markdown(f"**{member['name']}**")
+                    st.markdown(f"**Full-Stack Developer**")
+                    st.markdown(f"**    **")
 
 
 
